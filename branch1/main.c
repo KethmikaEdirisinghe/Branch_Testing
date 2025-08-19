0e2b1769fe673143ce86787e190c675b6f10dcba
Kethmika Edirisinghe <kethmikae@gmail.com>
2025-08-19 21:45:07 +0530

    test

--- branch1/main.c
+++ branch1/main.c
@@ -4,6 +4,10 @@
 int main()
 {
     printf("Hello world!\n");
-    printf("This is the first branch\n");s
+    printf("This is the first branch\n");
+
+    //edit in branch2
+    printf("This will be for branch2\n");
+
     return 0;
 }
