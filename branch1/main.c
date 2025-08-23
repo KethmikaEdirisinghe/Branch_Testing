783ad7afc461183467ea5f75475fbccafd709785
Kethmika Edirisinghe <kethmikae@gmail.com>
2025-08-24 02:43:03 +0530

    Adding

---
+++ branch1/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    printf("This is the first branch\n");
+
+    //edit in branch2
+    printf("This will be for branch2\n");
+
+    printf("This will be the next branch3\n");
+
+    return 0;
+}
